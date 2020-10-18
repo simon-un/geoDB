58af1967da35ae6cf86688be31db15ff81bd51e8
Oscar <osasanchezme@unal.edu.co>
2020-10-18 15:07:58 -0500

    Se sigue trabajando en la página del mapa
    Luego de tener el mapa llenando la pantalla y respodiendo a cambios de tamañao en la ventana se implementó una función para mostrar mensajes en la parte superior del mapa en caso de ser necesario.
    Se añade un boton en la esquina superior derecha para abrir el panel de atributos.

--- js/map.js
+++ js/map.js
@@ -43,11 +43,50 @@ auth.onAuthStateChanged(user => {
     })
 
 // Starting to work on the map
-document.getElementById('mapid').style.height = String(window.innerHeight - document.getElementById('navbarid').getBoundingClientRect().height)+'px';
-var map = L.map('mapid').setView([4.6384979,-74.082547], 16);
+const mapdiv = document.getElementById('mapid');
+const navbar = document.getElementById('navbarid');
+const alertnotif = document.getElementById('alert-notif');
+const optionsBtn = document.getElementById('sidebar-button');
+
+mapdiv.style.height = String(window.innerHeight - navbar.getBoundingClientRect().height)+'px';
+
+window.addEventListener('resize', (evt) => {
+    mapdiv.style.height = String(window.innerHeight - navbar.getBoundingClientRect().height)+'px';
+})
+
+alertnotif.addEventListener('click',()=>{
+    alertnotif.style.display = 'none';
+    // mapdiv.style.height = String(window.innerHeight - navbar.getBoundingClientRect().height)+'px';
+})
+
+const map = L.map('mapid').setView([4.6384979,-74.082547], 16);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var marker = L.marker([4.6384979,-74.082547]).addTo(map);
+const marker = L.marker([4.6384979,-74.082547]).addTo(map);
+
+showMsg = (msg, className = 'alert alert-primary') => {
+    // Options for className:
+    // - alert alert-secondary
+    // - alert alert-success
+    // - alert alert-danger
+    // - alert alert-warning
+    // - alert alert-info
+    // - alert alert-light
+    // - alert alert-dark
+    alertnotif.className = className;
+    document.getElementById('alertMsgP').innerHTML = msg;
+    alertnotif.style.display = 'block';
+}
+
+optionsBtn.addEventListener('click', () => {
+    window.alert("Here I am!")
+    optionsBtn.style.display = "none";
+})
+
+mapdiv.addEventListener('click', ()=>{
+    showMsg("Hizo click en el mapa", 'alert alert-info')
+    optionsBtn.style.display = "block";
+})
