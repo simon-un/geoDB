cfb7add5b3878ad2a212af8c5a3f3c4b58c9e134
dsvivass <dsvivass@unal.edu.co>
2020-10-29 19:06:25 -0500

    El archivo JSON ya cuenta con otro objeto llamado COORDSGEOJSON para la prueba de desempe\~no #14

---
+++ python_scripts/json2geojson.py
@@ -0,0 +1,28 @@
+import json
+
+# El archivo COORDS_SPOILER_BDG_FOPAE_2011.json ya cuenta con el objeto "COORDS"
+with open('../COORDS_SPOILER_BDG_FOPAE_2011.json', 'r') as f:
+    data = json.load(f)
+
+coords = {}
+coordsGeojson = {}
+
+new_data = {'EXPLORACIONES': data['EXPLORACIONES']}
+data = new_data
+
+data = data['EXPLORACIONES']
+
+for expl in data:
+    coords[expl] = {
+        'Norte' : data[expl]['Norte'],
+        'Este' : data[expl]['Este']
+    }
+    coordsGeojson[expl] = {
+        "type": "Point",
+        "coordinates": [data[expl]['Norte'], data[expl]['Este']]
+    }
+
+new_data['COORDS'] = coords
+new_data["COORDSGEOJSON"] = coordsGeojson
+with open('COORDS_SPOILER_BDG_FOPAE_2011.json', 'w') as file:
+    json.dump(new_data, file)
