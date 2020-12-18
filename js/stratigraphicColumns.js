bf815426542f882238804208adbe080df535ae27
dsvivass <dsvivass@unal.edu.co>
2020-12-17 23:03:01 -0500

    Se muestra informacion de los estratos al colocar el cursor sobre los rect #37

--- js/stratigraphicColumns.js
+++ js/stratigraphicColumns.js
@@ -3,6 +3,7 @@ function extractStratigraphicData(object) {
     var listDepth = []
     var min = null
     var max = null
+    var texts = []
     Object.keys(list).forEach(key => {
         listDepth.push({
             "top": list[key]['TRAMO_DESDE(m)'],
@@ -20,14 +21,18 @@ function extractStratigraphicData(object) {
         } else if (list[key]['TRAMO_DESDE(m)'] > max) {
             max = list[key]['TRAMO_DESDE(m)']
         }
+
+        texts.push({
+            "text": list[key]
+        })
     })
-    var texts = []
-        for (var i=1; i <= listDepth.length ; i++) {
-            texts.push({"text": `Texto${i}`})
-        }
-        console.log('t', texts)
-    nest = [{"key":object.properties.title, "values":listDepth, "texts":texts }]
-    console.log(nest)
+
+    nest = [{
+        "key": object.properties.title,
+        "values": listDepth,
+        "texts": texts
+    }]
+
     drawStratigraphicColumns(nest, min, max)
 }
 
@@ -47,95 +52,95 @@ function drawStratigraphicColumns(nest, min, max) {
     var g = svg.append("g")
         .attr("transform", translate(margin.left, margin.top));
 
-    // d3.csv("./js/data.csv", row, function (error, data) {
-        
+    var x = d3.scaleBand()
+        .domain(nest.map(function (d) {
+            return d.key;
+        }))
+        .range([0, width])
+        .padding(0.5);
 
-        var x = d3.scaleBand()
-            .domain(nest.map(function (d) {
-                return d.key;
-            }))
-            .range([0, width])
-            .padding(0.5);
+    var y = d3.scaleLinear()
+        .domain([max, min])
+        .range([height, 0]);
 
-        var y = d3.scaleLinear()
-            .domain([max, min])
-            .range([height, 0]);
+    // Create a group for each stack.
+    var stacks = g.append("g").selectAll(".stack")
+        .data(nest)
+        .enter().append("g")
+        .attr("class", "stack")
+        .attr("transform", function (d) {
+            return translate(x(d.key), 0);
+        });
 
-        // Create a group for each stack.
-        var stacks = g.append("g").selectAll(".stack")
-            .data(nest)
-            .enter().append("g")
-            .attr("class", "stack")
-            .attr("transform", function (d) {
-                return translate(x(d.key), 0);
+    var tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([0, 10])
+        .direction('e')
+        .html(function (d) {
+            var html = ''
+            Object.keys(d.text).forEach(key => {
+                html += `<strong>${key}:</strong> <span style='color:#118f8b'>${d.text[key]}</span><br>`
             });
+            return html
+        })
 
-        // Create a rectangle for each element.
-        stacks.selectAll(".element")
-            .data(function (d) {
-                return d.values;
-            })
-            .enter().append("rect")
-            .attr("class", "element")
-            .attr("cursor", "pointer")
-            .attr("x", 0)
-            .attr("y", function (d) {
-                return y(d.top);
-            })
-            .attr("width", x.bandwidth())
-            .attr("height", function (d) {
-                return Math.abs(y(d.top) - y(d.bottom));
-            });
+    svg.call(tip)
 
-// <button type="button" class="btn btn-secondary" title="" data-container="body" 
-      // data-toggle="popover" data-placement="bottom" 
-      // data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus." 
-      // data-original-title="Popover Title">Bottom</button>
-        stacks.append("button")
-            .attr("type", "button")
-            .attr("class", "btn btn-secondary")
-            .attr("title", "")
-            .attr("data-container", "body")
-            .attr("data-toggle", "popover")
-            .attr("data-placement", "bottom" )
-            .attr("data-content", "Vivamus sagittis lacus vel augue laoreet rutrum faucibus.")
-            .attr("data-original-title", "Popover Title")
 
-        // var texts = []
-        // for (var i=1; i <= nest[0]["values"].length ; i++) {
-        //     texts.push(`Texto${i}`)
-        // }
-        // console.log(texts)
+    // Create a rectangle for each element.
+    stacks.selectAll(".element")
+        .data(function (d) {
+            return d.values;
+        })
+        .enter().append("rect")
+        .attr("class", "element")
+        .attr("cursor", "pointer")
+        .attr("x", 0)
+        .attr("y", function (d) {
+            return y(d.top);
+        })
+        .attr("width", x.bandwidth())
+        .attr("height", function (d) {
+            return Math.abs(y(d.top) - y(d.bottom));
+        })
 
-        // Create text for each element.
-        stacks.selectAll(".elementText")
-            .data(function (d) {
-                return d.values;
-            })
-            .enter().append("text")
-            .attr("class", "elementText")
-            .attr("x", 0)
-            .attr("y", function (d) {
-                return y(d.top);
-            })
+    stacks.selectAll(".element")
+        .data(function (d) {
+            return d.texts;
+        })
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide);
 
-        stacks.selectAll(".elementText")
-            .data(function (d) {
-                return d.texts;
-            })
-            .text(function (d) {
-                return d.text;
-            })
+    stacks.append("button")
+        .attr("type", "button")
+        .attr("class", "btn btn-secondary")
+        .attr("title", "")
+        .attr("data-container", "body")
+        .attr("data-toggle", "popover")
+        .attr("data-placement", "bottom")
+        .attr("data-content", "Vivamus sagittis lacus vel augue laoreet rutrum faucibus.")
+        .attr("data-original-title", "Popover Title")
 
-        g.append("g")
-            .attr("class", "x axis")
-            .attr("transform", translate(0, height))
-            .call(d3.axisBottom(x));
+    // Create text for each element.
+    stacks.selectAll(".elementText")
+        .data(function (d) {
+            return d.values;
+        })
+        .enter().append("text")
+        .attr("class", "elementText")
+        .attr("x", 0)
+        .attr("y", function (d) {
+            return y(d.top);
+        })
 
-        g.append("g")
-            .attr("class", "y axis")
-            .call(d3.axisLeft(y));
-    // });
+    g.append("g")
+        .attr("class", "x axis")
+        .attr("transform", translate(0, height))
+        .call(d3.axisBottom(x));
+
+    g.append("g")
+        .attr("class", "y axis")
+        .call(d3.axisLeft(y));
 
     function row(d) {
         d.md = +d.md;
