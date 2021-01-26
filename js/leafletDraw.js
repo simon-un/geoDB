44656ae17c62d1e3426c21a110377d15a5c908a8
dsvivass <dsvivass@unal.edu.co>
2021-01-25 19:41:21 -0500

    Se agregan botones, y ya se puede graficar sobre el mapa #88

---
+++ js/leafletDraw.js
@@ -0,0 +1,67 @@
+// var LeafIcon = L.Icon.extend({
+//     options: {
+//         shadowUrl: 
+//             'http://leafletjs.com/docs/images/leaf-shadow.png',
+//         iconSize:     [38, 95],
+//         shadowSize:   [50, 64],
+//         iconAnchor:   [22, 94],
+//         shadowAnchor: [4, 62],
+//         popupAnchor:  [-3, -76]
+//     }
+// });
+
+// var greenIcon = new LeafIcon({
+//     iconUrl: 'http://leafletjs.com/docs/images/leaf-green.png'
+//     });
+
+var drawnItems = new L.FeatureGroup();
+map.addLayer(drawnItems);
+
+var drawControl = new L.Control.Draw({
+    position: 'bottomright',
+    draw: {
+        polygon: {
+            shapeOptions: {
+                color: 'purple'
+            },
+            allowIntersection: false,
+            drawError: {
+                color: 'orange',
+                timeout: 1000
+            },
+            showArea: true,
+            metric: false,
+            repeatMode: true
+        },
+        polyline: {
+            shapeOptions: {
+                color: 'red'
+            },
+        },
+        rect: {
+            shapeOptions: {
+                color: 'green'
+            },
+        },
+        circle: {
+            shapeOptions: {
+                color: 'steelblue'
+            },
+        },
+    },
+    edit: {
+        featureGroup: drawnItems
+    }
+});
+map.addControl(drawControl);
+
+map.on('draw:created', function (e) {
+    var type = e.layerType,
+        layer = e.layer;
+
+    if (type === 'marker') {
+        layer.bindPopup('A popup!');
+    }
+
+    drawnItems.addLayer(layer);
+});
